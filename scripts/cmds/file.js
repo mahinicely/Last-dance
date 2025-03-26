23a04310624c25b50a7c139e005065f2fc91da5c
RAZU <ice.cream.k9m@gmail.com>
2025-03-26 09:18:10 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -7,7 +7,7 @@ module.exports = {
 		version: "1.0",
 		author: "404",
 		countDown: 5,
-		role: 0,
+		role: 2,
 		shortDescription: "Send bot script",
 		longDescription: "Send bot specified file ",
 		category: "𝗢𝗪𝗡𝗘𝗥",
